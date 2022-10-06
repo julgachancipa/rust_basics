ed2a44bdca7905697987203ba6996b79cc99f9e4
Emma <julianaga01@gmail.com>
2022-10-06 16:01:07 -0500

    Variables en rust y cómo mostrarlas en pantalla

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let nombre: &str = "Juliana";
+    let mut age: u8 = 21;
+
+    age = age + 1;
+
+    println!("Hello my name is {} and I'm {} years old", age, nombre);
 }
