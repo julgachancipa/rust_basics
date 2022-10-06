dd56654080b5b386cf2ee448a56695ec9be0e1f1
Emma <julianaga01@gmail.com>
2022-10-06 16:05:22 -0500

    Negative variable

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     age = age + 1;
 
     println!("Hello my name is {} and I'm {} years old", age, nombre);
+
+    let min_temp: i8 = -5;
+    let max_temp: u8 = 10;
+
+    println!("min temp: {}", min_temp);
+    println!("max temp: {}", max_temp);
 }
